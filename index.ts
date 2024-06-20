6b420289a15c88b51c18777e39c6421e780a5760
Tooba Tehreem Sheikh <51713056+toobatehreem@users.noreply.github.com>
2024-06-20 20:38:41 +0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
+app.use(express.json());
 app.use(bodyParser.json());
 
 // Serve static files from the 'public' directory (if needed)
